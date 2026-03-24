422c532ae65d4ba81560f8e5c23febfb98297f26
Developer-Monicaben <164671122+Developer-Monicaben@users.noreply.github.com>
2026-03-24 19:30:04 +0530

    Update database password in DbSelect.py

--- DbSelect.py
+++ DbSelect.py
@@ -1,7 +1,7 @@
 import mysql.connector
 host="localhost"
 user="root"
-password="*30Pm28*"
+password="password"
 database="Pydb"
 
 conn=mysql.connector.connect(host=host,user=user,password=password,database=database)
